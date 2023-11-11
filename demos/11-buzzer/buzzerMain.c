6d96e51e052b8068cb1655981214b5de81c5c7ea
MayraRobles <mlrobles@miners.utep.edu>
2023-11-11 16:41:08 -0700

    Added interrupt for buttons

--- demos/11-buzzer/buzzerMain.c
+++ demos/11-buzzer/buzzerMain.c
@@ -48,3 +48,12 @@ __interrupt_vec(WDT_VECTOR) WDT ()
     break;
   }
 }
+
+/* Switches on P2 */
+void
+__interrupt_vec(PORT2_VECTOR) Port_2(){
+  if (P2IFG & SWITCHES) {      /* did a button cause this interrupt? */
+    P2IFG &= ~SWITCHES;      /* clear pending sw interrupts */
+    switch_interrupt_handler();/* single handler for all switches */
+  }
+}
